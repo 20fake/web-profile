b79fa77540a0eb064695053471199e119e09c64c
Muhammad Fathurrahman <45091626+20fake@users.noreply.github.com>
2024-01-31 22:24:14 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
-import DetailProject from "./components/DetailProject"
-import CardDetail from './components/CardDetail';
 
 function App() {
   return (
@@ -11,7 +9,6 @@ function App() {
       <Routes>
 
         <Route path='/' element={<Home />} />
-        <Route path='/detaildata' element={<CardDetail />} />
 
       </Routes>
 
